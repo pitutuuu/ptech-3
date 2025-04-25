4851cc4e2ebeb8ace53e504e30858369524e96ba
pitutuuu <sysio.olek@gmail.com>
2025-04-25 20:52:46 +0200

    feat: implement routing in App component and enhance Menu with navigation buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import  Menu  from "./components/Menu";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Posts from "./pages/Posts";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Menu from './components/Menu';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Posts from './pages/Posts';
 
 
 const App = () => {
     return (
         <>
-            <Menu />
-            <About />
-            <Contact />
-            <Posts />
+        <Menu />
+        <Routes>
+        <Route path="/" element={<Posts />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        </Routes>
         </>
     )
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,28 @@
-import { Typography } from "@mui/material";    
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";   
+import { useNavigate } from 'react-router-dom'
+import React from "react";
+
+const navItems = [
+  { label: "Home", path: "/" },
+  { label: "About", path: "/about" },
+  { label: "Contact", path: "/contact" },
+]
 
 const Menu = () => {
+  const navigate = useNavigate()
+
   return (
-    <Typography variant="h4">
-        Menu
-    </Typography>
+    <Box sx={{ display: "flex"}}>
+      <AppBar component={"nav"}>
+        <Toolbar>
+          {navItems.map((item) => (
+            <Button key={item.label} sx={{ color: "#fff" }} onClick={() => navigate(item.path)}>
+              {item.label}
+            </Button>
+          ))}
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
